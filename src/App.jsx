efba06067646ba97ef9f7050592d2f6ab3fe9adf
phibuinek <phibuinek@gmail.com>
2025-04-14 13:02:01 +0700

    base source, login, signup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import React from "react";
 import { Button } from "./components/ui/button";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Auth from "./page/auth";
+import Chat from "./page/chat";
+import Profile from "./page/profile";
 
 const App = () => {
   return (
-    <div>
-      <Button>Click me</Button>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/profile" element={<Profile />} />
+
+        <Route path="*" element={<Navigate to="/auth" />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
