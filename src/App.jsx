8a2602d5a87197d289d7d2595255a3d65eb1c13a
phibuinek <phibuinek@gmail.com>
2025-04-18 14:15:26 +0700

    zustand setup & jwt token verification & middleware

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,88 @@
-import { Button } from "./components/ui/button";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Auth from "./page/auth";
 import Chat from "./page/chat";
 import Profile from "./page/profile";
+import { useEffect, useState } from "react";
+import { useAppStore } from "./store";
+import { apiClient } from "./lib/api-client";
+import { GET_USER_INFO } from "./utils/constants";
+
+const PrivateRoute = ({ children }) => {
+  const { userInfo } = useAppStore();
+  const isAuthenticated = !!userInfo;
+  return isAuthenticated ? children : <Navigate to="/auth" />;
+};
+
+const AuthRoute = ({ children }) => {
+  const { userInfo } = useAppStore();
+  const isAuthenticated = !!userInfo;
+  return isAuthenticated ? <Navigate to="/chat" /> : children;
+};
 
 const App = () => {
+  const { userInfo, setUserInfo } = useAppStore();
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const getUserData = async () => {
+      try {
+        const response = await apiClient.get(GET_USER_INFO, {
+          withCredentials: true,
+        });
+        console.log("response after F5", response); // 👈 log tất cả response
+
+        // Kiểm tra thử in ra data
+        if (response.status === 200 && response.data.id) {
+          setUserInfo(response.data);
+          console.log("response.data", response.data);
+        } else {
+          setUserInfo(undefined);
+        }
+        console.log(response);
+      } catch (error) {
+        console.log(error);
+
+        setUserInfo(undefined);
+      } finally {
+        setLoading(false);
+      }
+    };
+    if (!userInfo) {
+      getUserData();
+    } else {
+      setLoading(false);
+    }
+  }, [userInfo, setUserInfo]);
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  console.log("userInfo", userInfo);
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/auth" element={<Auth />} />
-        <Route path="/chat" element={<Chat />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route
+          path="/auth"
+          element={
+            <AuthRoute>
+              <Auth />
+            </AuthRoute>
+          }
+        />
+        <Route
+          path="/chat"
+          element={
+            <PrivateRoute>
+              <Chat />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <PrivateRoute>
+              <Profile />
+            </PrivateRoute>
+          }
+        />
 
         <Route path="*" element={<Navigate to="/auth" />} />
       </Routes>
