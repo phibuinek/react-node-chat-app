04477b75519befa1338bedc49f6fbefea69e690a
phibuinek <phibuinek@gmail.com>
2025-04-22 14:04:13 +0700

    chat layout, empty chat screen, contacts container, chat container

--- src/page/chat/index.jsx
+++ src/page/chat/index.jsx
@@ -2,6 +2,9 @@ import { useAppStore } from "@/store";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "sonner";
+import ContactsContainer from "./components/contacts-container";
+import EmptyChatContainer from "./components/empty-chat-container";
+import ChatContainer from "./components/chat-container";
 
 const Chat = () => {
   const { userInfo } = useAppStore();
@@ -12,8 +15,14 @@ const Chat = () => {
       navigate("/profile");
     }
   }, [userInfo, navigate]);
-  
-  return <div>Chat</div>;
+
+  return (
+    <div className="flex h-[100vh] text-white overflow-hidden">
+      <ContactsContainer />
+      <EmptyChatContainer />
+      <ChatContainer />
+    </div>
+  );
 };
 
 export default Chat;
